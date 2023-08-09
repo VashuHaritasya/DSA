723ae6c992270d71f2554028d90822ceaab89c0a
Vashu Haritasya <vashusharma246@gmail.com>
2023-08-09 18:51:20 +0530

    switch

---
+++ src/com/vashu/Main.java
@@ -0,0 +1,40 @@
+package com.vashu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+//        if (fruits.equals("mango")){
+//            System.out.println("kings of fruits");
+//        }
+//        if (fruits.equals("apple")){
+//            System.out.println("a sweat red fruit");
+//        }
+
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("kings of fruits");
+//            case "apple" -> System.out.println("a sweat red fruit");
+//            case "Grapes" -> System.out.println("Small fruit");
+//            default -> System.out.println("please enter a valid fruit");
+//        }
+
+//        int day = in.nextInt();
+//        switch (day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("tuesday");
+//            case 3 -> System.out.println("wednesday");
+//            case 4 -> System.out.println("thursday");
+//            case 5 -> System.out.println("friday");
+//            case 6 -> System.out.println("saturday");
+//            case 7 -> System.out.println("sunday");
+//        }
+
+
+
+
+    }
+
+}
